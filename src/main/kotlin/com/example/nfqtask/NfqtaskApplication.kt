f1767d8cb30c2f58a0a2603332127ca15e40d251
Kyo <deividaspt@gmail.com>
2020-09-06 14:59:36 +0300

    Initial upload

---
+++ src/main/kotlin/com/example/nfqtask/NfqtaskApplication.kt
@@ -0,0 +1,11 @@
+package com.example.nfqtask
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NfqtaskApplication
+
+fun main(args: Array<String>) {
+    runApplication<NfqtaskApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/nfqtask/NfqtaskApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.nfqtask
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NfqtaskApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
